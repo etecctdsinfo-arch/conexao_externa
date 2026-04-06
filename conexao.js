98c72d82b1938d0e2d31d734db1f10c88b7278ff
etecctdsinfo-arch <etec.ct.ds.info@gmail.com>
2026-04-06 10:46:09 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -4,10 +4,10 @@ import mysql from 'mysql2/promise';
 async function conexao(){
     // Configurações do banco de dados
     const dbConfig = {
-        host: 'mysql-eversonbacelli.alwaysdata.net',
-        user: 'eversonbacelli',
-        password: 'Ln5TM.JWLyHA*x8',
-        database: 'eversonbacelli_projeto1',
+        host: '',
+        user: '',
+        password: '',
+        database: '',
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
